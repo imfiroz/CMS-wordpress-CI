42053992fdf3884539b8799efd313e4380e86388
firoz baksh <ferozbaksh3191@gmail.com>
2018-01-05 00:39:18 +0530

    Header add title and logo option added in admin view

--- application/views/admin/header/index.php
+++ application/views/admin/header/index.php
@@ -13,15 +13,46 @@ include_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . "../admin_header.php");
 	</div>
 	<?php endif; ?>
 	<div class="col-lg-7 col-md-3 col-sm-4">
-	<?php if(isset($header_data->title)): ?>
+	<h3>Header Title</h3>
+	<?php if(isset($header_data->title)): ?><!--checking title-->
 		<h1><?= $header_data->title ?></h1>
 	<?php else: ?>
-		<h1>Add title</h1>
+			<table class="table table-striped table-hover ">
+			  <thead>
+				<tr>
+				  <th>#</th>
+				  <th>Title</th>
+				  <th colspan="2">Action</th>
+				</tr>
+			  </thead>
+			  <tbody>
+				<tr>
+				  <td colspan="2"><p class="text-danger">Title not set yet.</p></td>
+				  <td><a href="#" class="btn btn-info btn-sm">Add Title</a></td>
+				</tr>
+			  </tbody>
+			</table> 
 	<?php endif; ?>
-	<?php if(isset($header_data->image_path)): ?>
+	<hr>
+	<h3>Header Logo</h3>
+	<?php if(isset($header_data->image_path)): ?><!--checking title-->
 		<h1><?= $header_data->image_path ?></h1>
 	<?php else: ?>
-		<h1>Add logo</h1>
+			<table class="table table-striped table-hover ">
+			  <thead>
+				<tr>
+				  <th>#</th>
+				  <th>Logo</th>
+				  <th colspan="2" >Action</th>
+				</tr>
+			  </thead>
+			  <tbody>
+				<tr>
+				  <td colspan="2"><p class="text-danger">Logo not set yet.</p></td>
+				  <td><a href="#" class="btn btn-info btn-sm">Add Logo</a></td>
+				</tr>
+			  </tbody>
+			</table> 
 	<?php endif; ?>
 	</div>
 	
